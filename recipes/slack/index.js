a5f724b91903526d8df9834c08b1398a84039600
Sadetdin EYILI <sad270@users.noreply.github.com>
2021-07-19 15:50:45 +0200

    Fix slack SSO with Google
    fix issue: https://github.com/getferdi/ferdi/issues/1350

--- recipes/slack/index.js
+++ recipes/slack/index.js
@@ -1,3 +1,17 @@
 "use strict";
 
-module.exports = Franz => Franz;
+module.exports = (Franz) =>
+    class Slack extends Franz {
+        modifyRequestHeaders() {
+            return [
+                {
+                    headers: {
+                        'user-agent': window.navigator.userAgent.replace(/(Ferdi|Electron)\/\S+( \([^)]+\))?/g, "").trim(),
+                    },
+                    requestFilters: {
+                        urls: ['*://*/*'],
+                    }
+                }
+            ]
+        }
+    };
